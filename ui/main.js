75231894557541ac13bc8af0eddbd4dbba20d016
Sukrut27 <sukrutdani.it@mmcoe.edu.in>
2016-09-29 02:30:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,17 @@
 console.log('Loaded!');
 
+// for new Value printing
 var element = document.getElementById('main.text');
 
-element.innerHTML = 'New value'; 
+element.innerHTML = 'Sukrut\'s Webapp';
+
+//for moving picture
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft+ 1;
+    img.style.marginLeft = margingLeft + 'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight, 50);
+};
