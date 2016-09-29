dde37b0dcd317476b5fd63cf848b786ed15aff6d
Sukrut27 <sukrutdani.it@mmcoe.edu.in>
2016-09-30 01:31:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 // for new Value printing
 var element = document.getElementById('main.text');
@@ -14,4 +14,38 @@ function moveRight() {
 }
 img.onclick = function(){
     var interval = setInterval(moveRight, 50);
+};*/
+
+// Counter code
+var button = document.getElementById('counter');
+button.onclick = function(){
+    // Create a req object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response & storeit in variable
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.Done){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://sukrut27.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
+
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    var names = ['name1','name2','name3','name4'];
+    var list ='';
+    fot(var i=0; i< names.length; i++) {
+        list =='<li>' + name[i] + '</li>';
+}
+var ul = document.getElementById('namelist');
+ul.innerHTML = list;
+}
