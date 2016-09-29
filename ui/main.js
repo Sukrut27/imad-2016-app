1d2a2b1a25a4a8bc7f496d17e641cf810cccf4db
Sukrut27 <sukrutdani.it@mmcoe.edu.in>
2016-09-30 02:13:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ img.onclick = function(){
 
 // Counter code
 var button = document.getElementById('counter');
-button.onclick = function() {
+button.onclick = function(){
     // Create a req object
     var request = new XMLHttpRequest();
     
@@ -41,14 +41,26 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    var names = ['name1','name2','name3','name4'];
-    var list = '' ;
-    for(var i=0; i< names.length; i++) {
-        list +='<li>' + names[i] + '</li>';
-    }
+      // Create a req object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response & storeit in variable
+    request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names = request.responseText;
+                names =JSON.parse(names);
+                var list = '' ;
+                for(var i=0; i< names.length; i++) {
+                      list =='<li>' + names[i] + '</li>';
+                }
 var ul = document.getElementById('namelist');
 ul.innerHTML = list;
-};
-
-
-
+            }
+        }
+    };
+    request.open('GET','http://sukrut27.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+    
+  
+}
